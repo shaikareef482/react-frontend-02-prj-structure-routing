036858da1ea7a5384b3ede33242ce10dd9835092
shaikareef081 <shaikareef482@gmail.com>
2023-02-11 22:09:11 +0530

    first commit

---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { useForm } from '../../shared/hooks/form-hook';
+
+import Input from '../../shared/Components/FormElements/Input';
+import Button from '../../shared/Components/FormElements/Button';
+import ErrorModal from '../../shared/Components/UIElements/ErrorModel';
+import ImageUpload from '../../shared/Components/FormElements/ImageUpload';
+import LoadingSpinner from '../../shared/Components/UIElements/LoadingSpinner';
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH
+} from '../../shared/util/validators';
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import { AuthContext } from '../../shared/context/auth-context';
+import './PlaceForm.css';
+import { useHistory } from 'react-router-dom';
+import { useContext } from 'react';
+
+
+
+const NewPlace = () => {
+  const auth = useContext(AuthContext);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: '',
+        isValid: false
+      },
+      description: {
+        value: '',
+        isValid: false
+      },
+      address: {
+        value: '',
+        isValid: false
+      },
+      image: {
+        value: null,
+        isValid: false
+      }
+    },
+    false
+  );
+
+  const history = useHistory();
+
+  const placeSubmitHandler = async event => {
+    event.preventDefault();
+    try {
+      const formData = new FormData();
+      formData.append('title', formState.inputs.title.value);
+      formData.append('description', formState.inputs.description.value);
+      formData.append('address', formState.inputs.address.value);
+      formData.append('image', formState.inputs.image.value);
+      await sendRequest('http://localhost:5000/api/places', 'POST', formData, {
+        Authorization: 'Bearer ' + auth.token
+      });
+      history.push('/');
+    } catch (err) {}
+  };
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      <form className="place-form" onSubmit={placeSubmitHandler}>
+        {isLoading && <LoadingSpinner asOverlay />}
+        <Input
+          id="title"
+          element="input"
+          type="text"
+          label="Title"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid title."
+          onInput={inputHandler}
+        />
+        <Input
+          id="description"
+          element="textarea"
+          label="Description"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter a valid description (at least 5 characters)."
+          onInput={inputHandler}
+        />
+        <Input
+          id="address"
+          element="input"
+          label="Address"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid address."
+          onInput={inputHandler}
+        />
+        <ImageUpload
+          id="image"
+          onInput={inputHandler}
+          errorText="Please provide an image."
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          ADD PLACE
+        </Button>
+      </form>
+    </React.Fragment>
+  );
+};
+
+export default NewPlace;
